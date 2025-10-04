3d8e4968cee15b95d208d7cdfe653b1a0cb09f86
Yara Shobut <yarashobut2@gmail.com>
2025-10-04 00:22:52 -0500

    create even page

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -5,7 +5,9 @@ import LoginPage from './components/pages/LoginPage';
 import Profile from './components/pages/Profile';
 import SurveyForm from './components/pages/SurveyForm';
 import Dashboard from './components/pages/Dashboard';
+import CreateEventPage from "./components/pages/CreateEventPage.jsx";
 import './App.css';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/survey" element={<SurveyForm />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-event" element={<CreateEventPage />} />
         </Routes>
       </div>
     </Router>
